ecc5c8e9d640461bada1261c63da85bafbd8e6b0
Oleg Pudeyev <oleg@bsdpower.com>
2010-05-29 21:06:04 +0000

    working support for testing jinja and erb templates

--- test/run_tests.py
+++ test/run_tests.py
@@ -3,8 +3,8 @@ import os, os.path, sys, unittest
 root = os.path.join(os.path.dirname(__file__), '..')
 sys.path.append(os.path.join(root, 'tools'))
 
-def run_tests(source_dir):
-    print 'Testing %s' % source_dir
+def run_tests(source_dir, template):
+    print 'Testing %s in %s' % (template, source_dir)
     if os.fork():
         pid, status = os.wait()
         if status > 0 and status < 256:
@@ -14,10 +14,9 @@ def run_tests(source_dir):
     else:
         full_source_path = os.path.join(root, source_dir)
         sys.path.append(full_source_path)
-        import tests.jinja
-        unittest.main(tests.jinja)
-        import tests.erb
-        unittest.main(tests.erb)
+        module = getattr(__import__('tests.%s' % template), template)
+        unittest.main(module)
 
-run_tests('src')
-run_tests('build')
+for source_dir in ('src', 'build'):
+    for template in ('jinja', 'erb'):
+        run_tests(source_dir, template)
